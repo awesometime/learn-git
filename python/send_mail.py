e59c27bce9d47c037862f6e5fab52abaffbeb0bb
awesometime <31882243+awesometime@users.noreply.github.com>
2018-07-13 16:45:37 +0800

    Update send_mail.py

--- python/send_mail.py
+++ python/send_mail.py
@@ -14,7 +14,7 @@ def send_mail():
     msg = MIMEText('这是一封测试邮件' + '\n' + '此处填写邮件正文内容'+ '\n'*5 +'Less is more', 'plain', 'utf-8') # 此处可以使  html,plain格式
     msg['From'] =  my_sender   #  括号里的对应发件人邮箱昵称、发件人邮箱账号
     msg['To'] = ";".join(my_receiver)  # 多个收件人,注意用分号
-    #msg['To'] = my_receiver   单个收件人
+    #msg['To'] = my_receiver   若为单个收件人，注意将 my_receiver = ['24xxxx28@qq.com'] 改为 my_receiver = '24xxxx28@qq.com'
     msg['Subject'] = "Hi,辛,早上好.发布任务---今天是%s" % (today_date)  # 邮件的主题
 
     server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是465
@@ -22,7 +22,7 @@ def send_mail():
     server.sendmail(my_sender, my_receiver, msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件内容
     server.quit()  # 关闭连接
     # 注意：
-	# MIMEText()["to"]的数据类型为str类型,多个地址使用逗号分隔
+    # MIMEText()["to"]的数据类型为str类型,多个地址使用逗号分隔
     # sendmail(from_addrs, to_addrs, ...)的to_addrs为list类型。
     print("邮件发送success")
 
