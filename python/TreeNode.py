742d940ec2c656ac67ed7dda409f5abb67577976
awesometime <31882243+awesometime@users.noreply.github.com>
2018-07-25 19:14:21 +0800

    Create TreeNode.py

---
+++ python/TreeNode.py
@@ -0,0 +1,98 @@
+###
+二叉树的产生及遍历
+
+
+class TreeNode:
+    def __init__(self, value=None, left=None, right=None):
+        self.value = value
+        self.left = left  # 左子树
+        self.right = right  # 右子树
+
+root = TreeNode('D', TreeNode('B', TreeNode('A'), TreeNode('C')), TreeNode('E', right=TreeNode('G', TreeNode('F'))))
+print(root.value)    #D
+print(root.left.value)   #B
+print(root.left.left.value)   #A
+print(root.right)  # 地址
+###
+
+class Node:  
+     def __init__(self,value=None,left=None,right=None):
+        self.value=value
+        self.left=left    #左子树
+        self.right=right  #右子树
+
+
+def preTraverse(root):
+    '''
+    前序遍历
+    '''
+    if root == None:
+        return
+    print(root.value)
+    preTraverse(root.left)
+    preTraverse(root.right)
+
+
+def midTraverse(root):
+    '''
+    中序遍历
+    '''
+    if root == None:
+        return
+    midTraverse(root.left)
+    print(root.value)
+    midTraverse(root.right)
+
+def afterTraverse(root):
+    '''
+    后序遍历
+    '''
+    if root == None:
+        return
+    afterTraverse(root.left)
+    afterTraverse(root.right)
+    print(root.value)
+
+
+if __name__=='__main__':
+    root=Node('D',Node('B',Node('A'),Node('C')),Node('E',right=Node('G',Node('F'))))
+    print('前序遍历：')
+    preTraverse(root)
+    print('\n')
+    print('中序遍历：')
+    midTraverse(root)
+    print('\n')
+    print('后序遍历：')
+    afterTraverse(root)
+    print('\n')
+    
+###
+前序遍历：
+D
+B
+A
+C
+E
+G
+F
+
+
+中序遍历：
+A
+B
+C
+D
+E
+F
+G
+
+
+后序遍历：
+A
+C
+B
+F
+G
+E
+D
+###
