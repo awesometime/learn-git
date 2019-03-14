e5e5417e28365ac3fd5cf09f125f8bfce3a581e1
awesometime <31882243+awesometime@users.noreply.github.com>
2019-03-14 18:01:55 +0800

    Update insert_sort.py

--- Data Structure and Algorithm/Data Structure/Sorted_Search/insert_sort.py
+++ Data Structure and Algorithm/Data Structure/Sorted_Search/insert_sort.py
@@ -4,12 +4,14 @@ def insertionSort(alist):
     global count
     for index in range(1, len(alist)):
         # 假设index=5,前5个已经正确排序，第6个即alist[5] 插入排好的子序列中
-        currentvalue = alist[index]
-        position = index
+        currentvalue = alist[index]  # 关键 将alist[index]赋值给一个变量
+        position = index             # 小tricky   position currenrvalue 【】
 
         # 前5个倒着分别与第6个比较，如果大的话放到最后
         # position-1
         while position > 0 and alist[position - 1] > currentvalue:
+        # while position > 0 and alist[position - 1] > alist[index]:  是错的   alist[index]已经修改了 
+        # [54, 54, 93, 93, 93, 93, 93, 93, 93]  会出现这种情况
             alist[position] = alist[position - 1]
             count += 1
             position = position - 1
