5ce46fd0b7ef0031d47e8320310915515099df27
awesometime <31882243+awesometime@users.noreply.github.com>
2019-03-14 17:43:26 +0800

    Update and rename select_sort.py to selection_sort.py

--- Data Structure and Algorithm/Data Structure/Sorted_Search/selection_sort.py
+++ Data Structure and Algorithm/Data Structure/Sorted_Search/selection_sort.py
@@ -35,3 +35,22 @@ print(selection_sort([7, 6, 58, 3, 2, 9, 10]))
 [2, 3, 6, 7, 9, 10, 58]
 
 [2, 3, 6, 7, 9, 10, 58]
+
+# 2
+def selectionSort(alist):
+    co = 0
+    for fillslot in range(len(alist) - 1, 0, -1):
+        positionOfMax = 0
+        for location in range(1, fillslot + 1):
+            if alist[location] > alist[positionOfMax]:
+                positionOfMax = location
+        alist[fillslot], alist[positionOfMax] = alist[positionOfMax], alist[fillslot]
+        co +=1
+        # temp = alist[fillslot]
+        # alist[fillslot] = alist[positionOfMax]
+        # alist[positionOfMax] = temp
+    return co
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+print(selectionSort(alist))
+print(alist)
