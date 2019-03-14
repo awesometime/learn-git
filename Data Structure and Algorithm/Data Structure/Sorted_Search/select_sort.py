ae8c10d11486bd0e4377a858dc6ebcb3b29e517c
awesometime <31882243+awesometime@users.noreply.github.com>
2019-03-14 17:41:04 +0800

    Rename selection_sort.py to select_sort.py

---
+++ Data Structure and Algorithm/Data Structure/Sorted_Search/select_sort.py
@@ -0,0 +1,37 @@
+# 1、时间复杂度：O(n2)  2、空间复杂度：O(1)  3、非稳定排序  4、原地排序
+# Python program for implementation of Selection Sort
+# https://www.geeksforgeeks.org/selection-sort/
+
+# 一次 loop 后最小的到最左边
+def selection_sort(li):
+    # Traverse through all array elements 
+    for i in range(len(li)):
+
+        # Find the minimum element in remaining 
+        # unsorted array 
+        min_idx = i
+        for j in range(i + 1, len(li)):
+            if li[min_idx] > li[j]:
+                min_idx = j
+
+        # Swap the found minimum element with the first element
+        li[i], li[min_idx] = li[min_idx], li[i]   # 注意缩进 一定要放在第二层循环外边
+        #print(li)
+        # temp = li[i]
+        # li[i] = li[min_idx]
+        # li[min_idx] = temp
+
+    return li
+
+
+print(selection_sort([7, 6, 58, 3, 2, 9, 10]))
+
+[2, 6, 58, 3, 7, 9, 10]
+[2, 3, 58, 6, 7, 9, 10]
+[2, 3, 6, 58, 7, 9, 10]
+[2, 3, 6, 7, 58, 9, 10]
+[2, 3, 6, 7, 9, 58, 10]
+[2, 3, 6, 7, 9, 10, 58]
+[2, 3, 6, 7, 9, 10, 58]
+
+[2, 3, 6, 7, 9, 10, 58]
