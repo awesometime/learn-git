e52bf9346dc7e0119f9308d55cc36cad163c72cd
awesometime <31882243+awesometime@users.noreply.github.com>
2019-03-28 11:46:50 +0800

    Update merge_sort.py

--- Data Structure and Algorithm/Data Structure/Sorted_Search/merge_sort.py
+++ Data Structure and Algorithm/Data Structure/Sorted_Search/merge_sort.py
@@ -1,12 +1,14 @@
 """
-归并排序:  https://www.geeksforgeeks.org/divide-and-conquer/
+归并排序: recursive   https://www.geeksforgeeks.org/merge-sort/
+         iterative   https://www.geeksforgeeks.org/iterative-merge-sort/
+
 如果列表有多个项，我们分割列表，并递归调用两个半部分的合并排序。
 一旦对这两半排序完成，就执行称为合并的基本操作。
 合并是获取两个较小的排序列表并将它们组合成单个排序的新列表的过程。
 """
 
 
-class Merge():
+class recursiveMergeSort():
     def __init__(self):
         pass
 
@@ -136,11 +138,98 @@ class Merge():
         result += right_list[right_pointer:]
         return result
 
+class iterativeMergeSort():
+    # Iterative Merge sort (Bottom Up) 
+
+    # Iterative mergesort function to 
+    # sort arr[0...n-1] 
+    def mergeSort(a): 
+
+        current_size = 1
+
+        # Outer loop for traversing Each 
+        # sub array of current_size 
+        while current_size < len(a) - 1: 
+
+            left = 0
+            # Inner loop for merge call 
+            # in a sub array 
+            # Each complete Iteration sorts 
+            # the iterating sub array 
+            while left < len(a)-1: 
+
+                # mid index = left index of 
+                # sub array + current sub 
+                # array size - 1 
+                mid = left + current_size - 1
+
+                # (False result,True result) 
+                # [Condition] Can use current_size 
+                # if 2 * current_size < len(a)-1 
+                # else len(a)-1 
+                right = ((2 * current_size + left - 1, 
+                        len(a) - 1)[2 * current_size 
+                            + left - 1 > len(a)-1]) 
+
+                # Merge call for each sub array 
+                merge(a, left, mid, right) 
+                left = left + current_size*2
+
+            # Increasing sub array size by 
+            # multiple of 2 
+            current_size = 2 * current_size 
+
+    # Merge Function 
+    def merge(a, l, m, r): 
+        n1 = m - l + 1
+        n2 = r - m 
+        L = [0] * n1 
+        R = [0] * n2 
+        for i in range(0, n1): 
+            L[i] = a[l + i] 
+        for i in range(0, n2): 
+            R[i] = a[m + i + 1] 
+
+        i, j, k = 0, 0, l 
+        while i < n1 and j < n2: 
+            if L[i] > R[j]: 
+                a[k] = R[j] 
+                j += 1
+            else: 
+                a[k] = L[i] 
+                i += 1
+            k += 1
+
+        while i < n1: 
+            a[k] = L[i] 
+            i += 1
+            k += 1
+
+        while j < n2: 
+            a[k] = R[j] 
+            j += 1
+            k += 1
+
+
+    # Driver code 
+    a = [12, 11, 13, 5, 6, 7] 
+    print("Given array is ") 
+    print(a) 
+
+    mergeSort(a) 
+
+    print("Sorted array is ") 
+    print(a) 
+
+    # Contributed by Madhur Chhangani [RCOEM] 
+
+   
 
 if __name__ == "__main__":
+    # recursiveMergeSort
     # 第一种
     alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    m = Merge()
+    m = recursiveMergeSort()
     m.mergeSort(alist)
     print(alist)
 
@@ -149,3 +238,7 @@ if __name__ == "__main__":
     rst = m.merge_sort_bizhan(alist2)
     print(alist2)
     print(rst)
+
+    # iterativeMergeSort
+    
+    
