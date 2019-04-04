f3bb3b9cb003023638251a5894dca576ed16b9dc
awesometime <31882243+awesometime@users.noreply.github.com>
2019-04-04 11:25:43 +0800

    Create ntree.py

---
+++ Data Structure and Algorithm/Data Structure/Tree/NTree/ntree.py
@@ -0,0 +1,157 @@
+event_list = [{'logid': 3, 'ibv': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 09:05:39.018', 'processguid': '{4AA00B44-2C2F-5C83-0000-00102F730A00}',
+               'parentprocessguid': None},
+              {'logid': 4, 'ibv': [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 09:06:21.320', 'processguid': '{4AA00B44-768D-5C87-0000-0010EA1EE400}',
+               'parentprocessguid': '{4AA00B44-2C2F-5C83-0000-00102F730A00}', 'updateid': [5, 7, 8],
+               'updatetime': ['2019-03-10 06:14:03.459', '2019-03-12 09:06:44.143', '2019-03-12 09:06:44.143']},
+              {'logid': 6, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 09:06:44.143', 'processguid': '{4AA00B44-76A4-5C87-0000-00109ED1E400}',
+               'parentprocessguid': '{4AA00B44-768D-5C87-0000-0010EA1EE400}'},
+              {'logid': 9, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 09:06:45.094', 'processguid': '{4AA00B44-76A5-5C87-0000-001071F8E400}',
+               'parentprocessguid': '{4AA00B44-768D-5C87-0000-0010EA1EE400}'},
+              {'logid': 10, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+               'utctime': '2019-03-12 09:06:46.015', 'processguid': '{4AA00B44-76A6-5C87-0000-00104B38E500}',
+               'parentprocessguid': '{4AA00B44-76A5-5C87-0000-001071F8E400}'},
+              {'logid': 11, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 09:06:46.218', 'processguid': '{4AA00B44-76A6-5C87-0000-0010D540E500}',
+               'parentprocessguid': '{4AA00B44-76A5-5C87-0000-001071F8E400}'},
+              {'logid': 12, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+               'utctime': '2019-03-12 09:06:46.467', 'processguid': '{4AA00B44-76A6-5C87-0000-0010E74AE500}',
+               'parentprocessguid': '{4AA00B44-76A5-5C87-0000-001071F8E400}'},
+              {'logid': 13, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 09:06:46.592', 'processguid': '{4AA00B44-76A6-5C87-0000-00103452E500}',
+               'parentprocessguid': '{4AA00B44-76A5-5C87-0000-001071F8E400}'},
+              {'logid': 14, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 09:06:47.840', 'processguid': '{4AA00B44-76A7-5C87-0000-0010CA96E500}',
+               'parentprocessguid': '{4AA00B44-76A5-5C87-0000-001071F8E400}'},
+              {'logid': 15, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 09:07:09.665', 'processguid': '{4AA00B44-76BD-5C87-0000-001040A5E800}',
+               'parentprocessguid': '{4AA00B44-768D-5C87-0000-0010EA1EE400}'},
+              {'logid': 16, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+               'utctime': '2019-03-12 09:07:09.758', 'processguid': '{4AA00B44-76BD-5C87-0000-00106CACE800}',
+               'parentprocessguid': '{4AA00B44-76BD-5C87-0000-001040A5E800}'},
+              {'logid': 161, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+               'utctime': '2019-03-12 09:07:09.758', 'processguid': '{4AA00B44-76BD-5C87-0000-00116CACE800}',
+               'parentprocessguid': '{4AA00B44-76BD-5C87-0000-00106CACE800}'},
+              {'logid': 162, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+               'utctime': '2019-03-12 09:07:09.758', 'processguid': '{4AA00B44-76BD-5C87-0000-00136CACE800}',
+               'parentprocessguid': '{4AA00B44-76BD-5C87-0000-00106CACE800}'},
+              {'logid': 1611, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+               'utctime': '2019-03-12 09:07:09.758', 'processguid': '{4AA00B44-76BD-5C87-0000-02156CACE800}',
+               'parentprocessguid': '{4AA00B44-76BD-5C87-0000-00116CACE800}'},
+              {'logid': 17, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 09:07:09.821', 'processguid': '{4AA00B44-76BD-5C87-0000-00108EB0E800}',
+               'parentprocessguid': '{4AA00B44-76BD-5C87-0000-001040A5E800}'},
+              {'logid': 18, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+               'utctime': '2019-03-12 09:07:09.930', 'processguid': '{4AA00B44-76BD-5C87-0000-00101DB6E800}',
+               'parentprocessguid': '{4AA00B44-76BD-5C87-0000-001040A5E800}'},
+              {'logid': 19, 'ibv': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 09:07:10.008', 'processguid': '{4AA00B44-76BE-5C87-0000-001074BBE800}',
+               'parentprocessguid': '{4AA00B44-76BD-5C87-0000-001040A5E800}'},
+              {'logid': 20, 'ibv': [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 10:51:42.841', 'processguid': '{4AA00B44-8F3E-5C87-0000-0010DD3D2301}',
+               'parentprocessguid': None, 'updateid': [21],
+               'updatetime': ['2019-03-12 10:51:51.093']},
+              {'logid': 21, 'ibv': [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               'utctime': '2019-03-12 10:51:42.841', 'processguid': '{4AA00B44-8F3E-5C87-0000-0010DD3D2301}',
+               'parentprocessguid': '{4AA00B44-8F3E-5C87-0000-0010DD3D2301}', 'updateid': [21],
+               'updatetime': ['2019-03-12 10:51:51.093']},
+              ]
+
+
+class NAryTreeNode():
+    def __init__(self, e):  # (e,children=[])不对
+        self.id = e['logid']
+        self.data = e  # dict
+        self.children = []  # list
+
+    # 添加一个孩子节点
+    def add_children(self, node):
+        self.children.append(node)
+
+
+class Process():
+    def __init__(self, event):
+        self.event = event
+
+    def preorder_tree(self, root):
+        if not root:
+            return []
+        res = []
+        res.append(root)
+        for child in root.children:
+            res.extend(self.preorder_tree(child))
+        return res
+
+    def build_tree(self):
+        root_nodes_list = []
+        for e in self.event:
+            if not e:
+                return
+            if e['parentprocessguid'] is None:
+                root = NAryTreeNode(e)
+                root_nodes_list.append(root)
+                continue
+            for root in root_nodes_list:
+                all_nodes = self.preorder_tree(root)
+                for node_index in range(len(all_nodes)):
+                    if e['parentprocessguid'] == all_nodes[node_index].data['processguid']:
+                        child_node = NAryTreeNode(e)
+                        all_nodes[node_index].add_children(child_node)
+        return root_nodes_list
+
+    def preorder_show_tree(self, root, depth):
+        if not root:
+            return
+        depth += 1
+        for child_index in range(len(root.children)):
+            if child_index == len(root.children) - 1:
+                print('|   ' * (depth - 1) + '└──' + str(root.children[child_index].id))
+            else:
+                print('|   ' * (depth - 1) + '├──' + str(root.children[child_index].id))
+            self.preorder_show_tree(root.children[child_index], depth)
+
+    def print_tree(self):
+        root_nodes_list = self.build_tree()
+        print(root_nodes_list)
+        for tree in self.build_tree():
+            print('\n' + 'Show tree, current tree.id is ' + str(tree.id))
+            print(tree.id)
+            self.preorder_show_tree(tree, 0)
+
+    def obv_tree(self):
+        pass
+
+
+if __name__ == '__main__':
+    pro = Process(event_list)
+    pro.build_tree()
+    pro.print_tree()
+
+
+# [<__main__.NAryTreeNode object at 0x0000000002C74898>, <__main__.NAryTreeNode object at 0x0000000002C748D0>]
+# 
+# Show tree, current tree.id is 3
+# 3
+# └──4
+# |   ├──6
+# |   ├──9
+# |   |   ├──10
+# |   |   ├──11
+# |   |   ├──12
+# |   |   ├──13
+# |   |   └──14
+# |   └──15
+# |   |   ├──16
+# |   |   |   ├──161
+# |   |   |   |   └──1611
+# |   |   |   └──162
+# |   |   ├──17
+# |   |   ├──18
+# |   |   └──19
+# 
+# Show tree, current tree.id is 20
+# 20
+# └──21
