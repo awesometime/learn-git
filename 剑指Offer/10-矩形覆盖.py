8b93a91b6a3294a836b43b55131aa5a275c8fbfc
awesometime <31882243+awesometime@users.noreply.github.com>
2019-04-19 15:40:39 +0800

    Update 10-矩形覆盖.py

--- 剑指Offer/10-矩形覆盖.py
+++ 剑指Offer/10-矩形覆盖.py
@@ -21,3 +21,11 @@ class Solution:
         for i in range(1,number):
             a,b = b,a+b
 		return b
+
+    # 没人用公式算	
+    def cl(n): 
+    	s = pow(5,0.5)
+    	# s1 = 5**0.5
+    	# print(s)
+    	# print(s1)
+    	return ((pow((1+s)//2, n+1) - pow((1+s)//2, n+1))//s)  # 公式不对
