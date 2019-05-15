4ae4ffadd0108adb30d13e17e31ee8637b6782f3
awesometime <31882243+awesometime@users.noreply.github.com>
2019-05-15 14:20:08 +0800

    Update 09_async_await.py

--- Python/python_advance/chapter12 协程和异步io/09_async_await.py
+++ Python/python_advance/chapter12 协程和异步io/09_async_await.py
@@ -1,7 +1,29 @@
 # python3.5 以前通过生成器实现协程
 # python3.5 为了将语义变得更加明确，就引入了async和await关键词用于定义原生的协程
 
-"""协程是单线程模式"""
+"""1 协程是单线程模式"""
+import threading
+import asyncio
+
+
+async def hello():
+    print('Hello world! (%s)' % threading.currentThread())
+    await asyncio.sleep(3)
+    print('Hello again! (%s)' % threading.currentThread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+
+# Hello world! (<_MainThread(MainThread, started 37128)>)
+# Hello world! (<_MainThread(MainThread, started 37128)>)
+# Hello again! (<_MainThread(MainThread, started 37128)>)
+# Hello again! (<_MainThread(MainThread, started 37128)>)
+
+
+
+# --------------------
 # async def downloader(url):
 #     return "bobby"
 import types
