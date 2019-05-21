f94b3647a7238122bcb84e90652ee6a7927a3127
awesometime <31882243+awesometime@users.noreply.github.com>
2019-05-21 17:20:16 +0800

    Update main.go

--- GoLang/learn_go/interface/main.go
+++ GoLang/learn_go/interface/main.go
@@ -104,3 +104,34 @@ func inspect(r Retriever) {
 	}
 	fmt.Println()
 }
+
+
+/*
+interface{}
+空接口里面没有方法，所以它也不具有任何能力，其作用相当于 Java 的 Object 类型，可以容纳任意对象，它是一个万能容器。
+比如一个字典的 key 是字符串，但是希望 value 可以容纳任意类型的对象，
+类似于 Java 语言的 Map 类型，这时候就可以使用空接口类型 interface{}。
+
+package main
+
+import "fmt"
+
+func main() {
+    // 连续两个大括号，是不是看起来很别扭
+    var user = map[string]interface{}{
+        "age": 30,
+        "address": "Beijing Tongzhou",
+        "married": true,
+    }
+    fmt.Println(user)
+    // 类型转换语法来了
+    var age = user["age"].(int)
+    var address = user["address"].(string)
+    var married = user["married"].(bool)
+    fmt.Println(age, address, married)
+}
+
+-------------
+map[age:30 address:Beijing Tongzhou married:true]
+30 Beijing Tongzhou true
+*/
