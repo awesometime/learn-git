48e148c9e3309d90ff96c93ce4ca1a9066002cf9
awesometime <31882243+awesometime@users.noreply.github.com>
2019-05-25 13:16:49 +0800

    Update fib.go

--- GoLang/learn_go/functional/fib/fib.go
+++ GoLang/learn_go/functional/fib/fib.go
@@ -1,5 +1,8 @@
-package fib
+package main
 
+import "fmt"
+
+// 函数式编程
 // 1, 1, 2, 3, 5, 8, 13, ...
 func Fibonacci() func() int {
 	a, b := 0, 1
@@ -7,4 +10,22 @@ func Fibonacci() func() int {
 		a, b = b, a+b
 		return a
 	}
+}
+
+func main() {
+	f := Fibonacci()
+
+	fmt.Println(f()) //Fibonacci()()
+	// Fibonacci()返回 func() int { a, b = b, a+b return a }
+	// Fibonacci()() == func()()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
 }
