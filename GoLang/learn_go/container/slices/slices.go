f697f2a0c2ebfeb82c82d04f7ceb439e297eccf7
awesometime <31882243+awesometime@users.noreply.github.com>
2019-05-25 10:40:39 +0800

    Update slices.go

--- GoLang/learn_go/container/slices/slices.go
+++ GoLang/learn_go/container/slices/slices.go
@@ -56,3 +56,58 @@ func main() {
 	fmt.Println("Uncomment to see sliceOps demo")
 	// sliceOps()
 }
+
+
+
+/*
+[理解Go数组和切片的内部实现原理](https://i6448038.github.io/2018/08/11/array-and-slice-principle/)
+
+
+// 关于数组扩容
+// 两条规则：
+// 1
+// 如果切片的容量小于1024个元素，那么扩容的时候slice的cap就翻番，乘以2；
+// 一旦元素个数超过1024个元素，增长因子就变成1.25，即每次增加原来容量的四分之一。
+// 2
+// 如果扩容之后，还没有触及原数组的容量，那么，切片中的指针指向的位置，就还是原数组，
+// 如果扩容之后，超过了原数组的容量，那么，Go就会开辟一块新的内存，
+// 把原来的值拷贝过来，这种情况丝毫不会影响到原数组
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	array := [4]int{10, 20, 30, 40}
+	slice := array[0:2]
+	fmt.Println(slice, len(slice),cap(slice))
+
+	newSlice := append(slice, 50)
+	newSlice[1] += 100
+	fmt.Println(newSlice, len(newSlice),cap(newSlice))
+
+	newSlice1 := append(newSlice, 100)
+	fmt.Println(newSlice1, len(newSlice1),cap(newSlice1))
+
+	newSlice2 := append(newSlice1, 150)
+	fmt.Println(newSlice2, len(newSlice2),cap(newSlice2))
+
+	newSlice2[1] += 1
+	fmt.Println(newSlice2, len(newSlice2),cap(newSlice2))
+
+	fmt.Println(slice, len(slice),cap(slice))
+	fmt.Println(array, len(array),cap(array))
+}
+
+
+//[10 20] 2 4
+//[10 120 50] 3 4
+//[10 120 50 100] 4 4
+//[10 120 50 100 150] 5 8
+//[10 121 50 100 150] 5 8
+//[10 120] 2 4
+//[10 120 50 100] 4 4
+
+*/
