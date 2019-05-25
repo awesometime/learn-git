8410fe2899f05e871570ae7a1969b9a40301959e
awesometime <31882243+awesometime@users.noreply.github.com>
2019-05-25 13:17:37 +0800

    Update adder.go

--- GoLang/learn_go/functional/adder/adder.go
+++ GoLang/learn_go/functional/adder/adder.go
@@ -1,7 +1,23 @@
+/*
+ 函数式编程 闭包
+*/
+
 package main
 
 import "fmt"
 
+/*
+python 闭包
+def adder():
+	sum := 0
+	def func(value):
+		nonlocal sum
+        sum += value
+		return sum
+    return func
+*/
+
+// 更好理解
 func adder() func(int) int {
 	sum := 0
 	return func(v int) int {
@@ -10,6 +26,7 @@ func adder() func(int) int {
 	}
 }
 
+// 严格的正统 函数式编程
 type iAdder func(int) (int, iAdder)
 
 func adder2(base int) iAdder {
@@ -19,12 +36,24 @@ func adder2(base int) iAdder {
 }
 
 func main() {
+	a0 := adder() // a0 == func(v int) int { sum += v return sum}  没名字函数
+	for i := 0; i < 10; i++ {
+		fmt.Printf("0 + 1 + ... + %d = %d\n", i, a0(i))
+	}
+	// a0() == func()
+
+	// 和上面不一样
+	for i := 0; i < 10; i++ {
+		fmt.Printf("0 + 1 + ... + %d = %d\n",
+			i, adder()(i))
+	}
+
+	// 正统的 函数式编程 但不好理解 没必要这样用
 	// a := adder() is trivial and also works.
 	a := adder2(0)
 	for i := 0; i < 10; i++ {
 		var s int
 		s, a = a(i)
-		fmt.Printf("0 + 1 + ... + %d = %d\n",
-			i, s)
+		fmt.Printf("0 + 1 + ... + %d = %d\n", i, s)
 	}
 }
