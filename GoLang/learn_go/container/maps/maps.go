8e896402e69b9b53c2f9158a53c38eba493af6c1
awesometime <31882243+awesometime@users.noreply.github.com>
2019-05-25 15:29:35 +0800

    Update maps.go

--- GoLang/learn_go/container/maps/maps.go
+++ GoLang/learn_go/container/maps/maps.go
@@ -1,3 +1,12 @@
+/*
+解剖Go语言map底层实现
+https://i6448038.github.io/2018/08/26/map-secret/
+*/
+
+
+/*
+除slice map function之外的内建类型都可以作为key
+*/
 package main
 
 import "fmt"
@@ -17,6 +26,8 @@ func main() {
 	fmt.Println("m, m2, m3:")
 	fmt.Println(m, m2, m3)
 
+	// 遍历
+	// 注意 map 是无序的 每次打印结果不同
 	fmt.Println("Traversing map m")
 	for k, v := range m {
 		fmt.Println(k, v)
@@ -25,19 +36,23 @@ func main() {
 	fmt.Println("Getting values")
 	courseName := m["course"]
 	fmt.Println(`m["course"] =`, courseName)
+	
+	// map中key不存在时 打印value相应类型的初始值
+	// 判断key是否存在
 	if causeName, ok := m["cause"]; ok {
 		fmt.Println(causeName)
 	} else {
 		fmt.Println("key 'cause' does not exist")
 	}
 
+	// 删除
 	fmt.Println("Deleting values")
-	name, ok := m["name"]
+	name, ok := m["name"]    //   :=
 	fmt.Printf("m[%q] before delete: %q, %v\n",
 		"name", name, ok)
 
 	delete(m, "name")
-	name, ok = m["name"]
+	name, ok = m["name"]     // ok 有了需要用  =
 	fmt.Printf("m[%q] after delete: %q, %v\n",
 		"name", name, ok)
 }
