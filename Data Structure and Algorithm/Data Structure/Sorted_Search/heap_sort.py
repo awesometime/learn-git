00573aaab35573a12e439de020a98d28d8621210
awesometime <31882243+awesometime@users.noreply.github.com>
2019-06-10 20:21:48 +0800

    Update heap_sort.py

--- Data Structure and Algorithm/Data Structure/Sorted_Search/heap_sort.py
+++ Data Structure and Algorithm/Data Structure/Sorted_Search/heap_sort.py
@@ -7,11 +7,6 @@
 # 
 # 父节点的值大于子节点    大顶堆
 #
-# step1: 依次排好相当于建了一个堆 但是不满足root大于子节点的条件
-# step2: 从最后一个节点的父节点开始调整堆 heapify()
-# step3: root(max)和最后一个节点交换,最后一个节点到root处，并下沉调整堆 heapify()
-#
-#
 # Python program for implementation of heap Sort
 
 # To heapify subtree rooted at index i.
@@ -80,6 +75,15 @@ for i in range(n):
 
 ###########################################
 # 版本2
+
+# step1: Build_MAX_Heap 
+#        从最后一个节点的父节点开始调整构建大顶堆 满足root大于子节点 不满足排序 子节点之间未排序而且不满足从小到大
+# step2: HeapSort
+#        root(max)和最后一个节点交换,root(max)到最后，调整除了最后一个节点的其余节点,具体是root比子节点小的话下沉
+# 具体调整当然是用 MAX_Heapify
+
+
+# 构造一个大顶堆 每次调整的过程
 def MAX_Heapify(heap, HeapSize, root):  # 在堆中做结构调整使得父节点的值大于子节点
 
     left = 2 * root + 1
@@ -94,14 +98,20 @@ def MAX_Heapify(heap, HeapSize, root):  # 在堆中做结构调整使得父节�
         MAX_Heapify(heap, HeapSize, larger)  # 从顶端递归往下调整，用larger是因为larger是数组索引，并且已经在比较时候更新过，而root没有更新过！
 
 
-def Build_MAX_Heap(heap):  # 构造一个堆，将堆中所有数据重新排序
+# 从最后一个根节点开始倒着循环构造一个大顶堆，这个堆只满足根比子节点大 并不满足 从上到下 从左到右依次排序
+def Build_MAX_Heap(heap):  
     HeapSize = len(heap)
     for i in range((HeapSize - 1) // 2, -1, -1):  # 从后往前出数(z最后一个节点的父节点)  '//' got integer
         MAX_Heapify(heap, HeapSize, i)  # 这里堆的大小是固定，root是i逐步减小
 
 
+# 排序
 def HeapSort(heap):  # 将根节点取出与最后一位做对调，对前面len-1个节点继续进行对调整过程。
-    Build_MAX_Heap(heap)
+    # 1 构建大顶堆 这个堆只满足根比子节点大  【注意】这个堆并未排序
+    Build_MAX_Heap(heap)     
+    
+    # 2 将堆顶元素(最大元素)放到堆尾,重新调整剩余节点，使其满足大顶堆
+    #   这样循环完 最大元素从末尾开始依次放置好 就排序完成
     for i in range(len(heap) - 1, -1, -1):
         heap[0], heap[i] = heap[i], heap[0]
         MAX_Heapify(heap, i, 0)  # 这里堆的大小是逐步缩小，root永远是0
